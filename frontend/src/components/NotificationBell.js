13897c7c2ee52f1d47ddc7fc321bbc56bfbfe2ed
StefanGrozde <stefan.grozdanovski0@gmail.com>
2025-07-14 22:16:06 +0200

    Fixing notifications build errors

--- frontend/src/components/NotificationBell.js
+++ frontend/src/components/NotificationBell.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { FaBell } from 'react-icons/fa';
 
 const NotificationBell = ({ onClick, unreadCount = 0 }) => {
   const [isAnimating, setIsAnimating] = useState(false);
@@ -25,7 +24,9 @@ const NotificationBell = ({ onClick, unreadCount = 0 }) => {
         `}
         aria-label={`Notifications ${unreadCount > 0 ? `(${unreadCount} unread)` : ''}`}
       >
-        <FaBell className="w-5 h-5" />
+        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path d="M10 2C8.14348 2 6.36301 2.7375 5.05025 4.05025C3.7375 5.36301 3 7.14348 3 9V11.586L1.293 13.293C1.199 13.387 1.127 13.499 1.08 13.622C1.033 13.745 1.01 13.877 1.01 14.01C1.01 14.143 1.033 14.275 1.08 14.398C1.127 14.521 1.199 14.633 1.293 14.727C1.387 14.821 1.499 14.893 1.622 14.94C1.745 14.987 1.877 15.01 2.01 15.01H6.05C6.25 16.92 7.92 18.41 10 18.41C12.08 18.41 13.75 16.92 13.95 15.01H17.99C18.123 15.01 18.255 14.987 18.378 14.94C18.501 14.893 18.613 14.821 18.707 14.727C18.801 14.633 18.873 14.521 18.92 14.398C18.967 14.275 18.99 14.143 18.99 14.01C18.99 13.877 18.967 13.745 18.92 13.622C18.873 13.499 18.801 13.387 18.707 13.293L17 11.586V9C17 7.14348 16.2625 5.36301 14.9497 4.05025C13.637 2.7375 11.8565 2 10 2Z"/>
+        </svg>
         
         {/* Notification badge */}
         {unreadCount > 0 && (
--- frontend/src/components/NotificationDropdown.js
+++ frontend/src/components/NotificationDropdown.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { FaCheck, FaTimes, FaTask, FaTicketAlt, FaCommentAlt, FaUserPlus, FaExchangeAlt, FaBell } from 'react-icons/fa';
 
 const NotificationDropdown = ({ 
   notifications = [], 
@@ -33,13 +32,33 @@ const NotificationDropdown = ({
     
     switch (type) {
       case 'assignment':
-        return <FaUserPlus className={iconClass} />;
+        return (
+          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
+            <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"/>
+          </svg>
+        );
       case 'status_change':
-        return <FaExchangeAlt className={iconClass} />;
+        return (
+          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd"/>
+          </svg>
+        );
       case 'comment':
-        return <FaCommentAlt className={iconClass} />;
+        return (
+          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd"/>
+          </svg>
+        );
       default:
-        return entityType === 'task' ? <FaTask className={iconClass} /> : <FaTicketAlt className={iconClass} />;
+        return entityType === 'task' ? (
+          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"/>
+          </svg>
+        ) : (
+          <svg className={iconClass} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" clipRule="evenodd"/>
+          </svg>
+        );
     }
   };
 
@@ -98,7 +117,9 @@ const NotificationDropdown = ({
           </div>
         ) : notifications.length === 0 ? (
           <div className="px-4 py-8 text-center text-gray-500">
-            <FaBell className="w-12 h-12 mx-auto mb-2 text-gray-300" />
+            <svg className="w-12 h-12 mx-auto mb-2 text-gray-300" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M10 2C8.14348 2 6.36301 2.7375 5.05025 4.05025C3.7375 5.36301 3 7.14348 3 9V11.586L1.293 13.293C1.199 13.387 1.127 13.499 1.08 13.622C1.033 13.745 1.01 13.877 1.01 14.01C1.01 14.143 1.033 14.275 1.08 14.398C1.127 14.521 1.199 14.633 1.293 14.727C1.387 14.821 1.499 14.893 1.622 14.94C1.745 14.987 1.877 15.01 2.01 15.01H6.05C6.25 16.92 7.92 18.41 10 18.41C12.08 18.41 13.75 16.92 13.95 15.01H17.99C18.123 15.01 18.255 14.987 18.378 14.94C18.501 14.893 18.613 14.821 18.707 14.727C18.801 14.633 18.873 14.521 18.92 14.398C18.967 14.275 18.99 14.143 18.99 14.01C18.99 13.877 18.967 13.745 18.92 13.622C18.873 13.499 18.801 13.387 18.707 13.293L17 11.586V9C17 7.14348 16.2625 5.36301 14.9497 4.05025C13.637 2.7375 11.8565 2 10 2Z"/>
+            </svg>
             <p>No notifications yet</p>
           </div>
         ) : (
@@ -134,7 +155,9 @@ const NotificationDropdown = ({
                             className="p-1 text-gray-400 hover:text-green-600 focus:outline-none"
                             title="Mark as read"
                           >
-                            <FaCheck className="w-3 h-3" />
+                            <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
+                              <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"/>
+                            </svg>
                           </button>
                         )}
                         
@@ -146,7 +169,9 @@ const NotificationDropdown = ({
                           className="p-1 text-gray-400 hover:text-red-600 focus:outline-none"
                           title="Delete notification"
                         >
-                          <FaTimes className="w-3 h-3" />
+                          <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
+                            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"/>
+                          </svg>
                         </button>
                       </div>
                     </div>
